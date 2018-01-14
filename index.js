158dc8dcb7103afe59d33528e0366dc0944f752f
Dave Swebilius <dswebilius@gmail.com>
2018-01-14 02:12:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-    return string.toUpperCase ()
+    return string.toUpperCase()
     console.log('hello')
 }
+logShout('hello')
